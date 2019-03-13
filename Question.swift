578807c6cf73ec0f74775cfe2c8073f805200e10
trekhleb123 <trekhleb.al@gmail.com>
2019-03-13 13:18:12 -0500

    initial commit

---
+++ Question.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+
+class Question {
+    let questonText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questonText = text
+        answer = correctAnswer
+    }
+}
+
+
